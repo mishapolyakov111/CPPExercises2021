84c8d5cee1a4d15cd4fb3e9e11a35cc2b424a9d5
kolipi579 <71726497+kolipi579@users.noreply.github.com>
2022-03-16 10:48:41 +0300

    1

--- lesson15/src/mainDijkstra.cpp
+++ lesson15/src/mainDijkstra.cpp
@@ -11,7 +11,7 @@ int debugPoint(int line) {
     return line;
 }
 
-#define rassert(condition, message) if (!(condition)) { std::stringstream ss; (ss << "Assertion \"" << message << "\" failed at line " << debugPoint(__LINE__) << "!"); throw std::runtime_error(ss.str()); }
+#define rassert(condition, message) if (!(condition)) { std::stringstream ss; (ss << "Assertion \"" << message << "\" failed at line " << debugPoint(LINE) << "!"); throw std::runtime_error(ss.str()); }
 
 
 struct Edge {
@@ -23,7 +23,9 @@ struct Edge {
 };
 
 void run() {
-    // https://codeforces.com/problemset/problem/20/C?locale=ru
+    // 
+
+
     // Не требуется сделать оптимально быструю версию, поэтому если вы получили:
     //
     // Превышено ограничение времени на тесте 31
@@ -38,13 +40,13 @@ void run() {
     for (int i = 0; i < medges; ++i) {
         int ai, bi, w;
         std::cin >> ai >> bi >> w;
-        rassert(ai >= 1 && ai <= nvertices, 23472894792020);
-        rassert(bi >= 1 && bi <= nvertices, 23472894792021);
+        //rassert(ai >= 1 && ai <= nvertices, 23472894792020);
+        //rassert(bi >= 1 && bi <= nvertices, 23472894792021);
 
         ai -= 1;
         bi -= 1;
-        rassert(ai >= 0 && ai < nvertices, 3472897424024);
-        rassert(bi >= 0 && bi < nvertices, 3472897424025);
+        //rassert(ai >= 0 && ai < nvertices, 3472897424024);
+        //rassert(bi >= 0 && bi < nvertices, 3472897424025);
 
         Edge edgeAB(ai, bi, w);
         edges_by_vertex[ai].push_back(edgeAB);
@@ -58,22 +60,43 @@ void run() {
     const int INF = std::numeric_limits<int>::max();
 
     std::vector<int> distances(nvertices, INF);
-    // TODO ...
 
-//    while (true) {
-//
-//    }
+    distances[start] = 0;
+    std::vector<bool> used(nvertices, false);
+    std::vector<int> father(nvertices, -1);
 
-//    if (...) {
-//        ...
-//        for (...) {
-//            std::cout << (path[i] + 1) << " ";
-//        }
-//        std::cout << std::endl;
-//    } else {
-//        std::cout << -1 << std::endl;
-//    }
+    while (!used[finish]) {
+        int n = -1;
+
+        for(int i = 0; i < nvertices; i++){
+            if(!used[i] && (distances[i] < INF)) n = i;
+        }
+
+        if(n == -1) break;
+
+        used[n] = true;
+
+        for(auto edge : edges_by_vertex[n]){
+            if (distances[edge.v] > distances[n] + edge.w){
+                distances[edge.v] = distances[n] + edge.w;
+                father[edge.v] = n;
+            }
+        }
+    }
+
+    if (distances[finish] != INF) {
+        std::vector<int> path;
+        int place = finish;
+        while(place != -1){
+            path.push_back(place);
+            place = father[place];
+        }
+        for (int i = path.size()-1; i >= 0; i--) {
+            std::cout << (path[i] + 1) << " ";
+        }
+    }
 }
+
 
 int main() {
     try {
